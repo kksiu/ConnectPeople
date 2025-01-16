75a269b310fc5fd2b2b3561480bc70d6fcc15ef3
Kenneth S. <kennethksiu@gmail.com>
2025-01-15 21:34:23 -0800

    Finally done

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,38 +2,46 @@ import type { ConfigContext, ExpoConfig } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "expo",
-  slug: "expo",
+  name: "connect-people",
+  slug: "connect-people",
   scheme: "expo",
   version: "0.1.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
+  runtimeVersion: {
+    policy: "appVersion",
+  },
   splash: {
     image: "./assets/icon.png",
     resizeMode: "contain",
     backgroundColor: "#1F104A",
   },
   updates: {
+    url: "https://u.expo.dev/93991b4f-6a48-4790-9f6e-144650621a6f",
     fallbackToCacheTimeout: 0,
   },
   assetBundlePatterns: ["**/*"],
   ios: {
-    bundleIdentifier: "your.bundle.identifier",
+    bundleIdentifier: "com.kks.connectpeople",
     supportsTablet: true,
   },
   android: {
-    package: "your.bundle.identifier",
+    package: "com.kks.connectpoeple",
     adaptiveIcon: {
       foregroundImage: "./assets/icon.png",
       backgroundColor: "#1F104A",
     },
   },
-  // extra: {
-  //   eas: {
-  //     projectId: "your-eas-project-id",
-  //   },
-  // },
+  owner: "kksiu",
+  extra: {
+    router: {
+      origin: false,
+    },
+    eas: {
+      projectId: "93991b4f-6a48-4790-9f6e-144650621a6f",
+    },
+  },
   experiments: {
     tsconfigPaths: true,
     typedRoutes: true,
